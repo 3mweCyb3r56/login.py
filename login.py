ea7a02fbf63f0f5cf3c7c01195db656b0c66e740
emwe56 <73535922+emwe56@users.noreply.github.com>
2020-11-04 20:40:42 +0700

    Add files via upload

---
+++ login.py
@@ -0,0 +1,23 @@
+python joi.py
+
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+NOW=`date "+%d.%m.%Y"`
+TIME=`date "+%H:%M"`
+echo
+neofetch
+echo -e $blue'======================================================'
+echo Login With...| lolcat
+echo -e $red'======================================================'
+echo Nama      : Jonathan Immanuel | lolcat
+echo Sekolah   : SMA Dharma Putra | lolcat
+echo Whatsapp  : +62 822-6068-5144 | lolcat
+echo -e $cyan'======================================================'
+date | lolcat
+echo -e $green'======================================================'
